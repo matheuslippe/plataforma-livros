b779825fc1f5fcdcd0b7cb7cf1706f6a4d73be1b
Matheus Lippe <matheuslippe6@gmail.com>
2026-05-25 10:27:34 -0300

    Correção no test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,16 +1,10 @@
-import pytest
-import sys
-import os
-
-# Adiciona o diretório raiz ao PYTHONPATH para encontrar o módulo 'app'
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from main import app
 from fastapi.testclient import TestClient
+from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
-    # FastAPI retorna 404 se não houver rota "/", o que confirma que a app subiu
+    # FastAPI retorna 404 se não houver rota "/", o que confirma que a app subiu sem erros
     assert response.status_code in [200, 404]
